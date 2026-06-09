3d3bd5001f51609770dd389140b2314b621bbc9d
Ivine Kelly Acho <ivinekellyacho@Ivines-MacBook-Pro.local>
2026-06-09 19:19:29 +0100

    added cloudinary dependency

--- app.config.js
+++ app.config.js
@@ -27,8 +27,8 @@ module.exports = {
     extra: {
       API_URL: "http://172.20.10.3:8000",
     },
-    "eas": {
-        "projectId": "YOUR_EAS_PROJECT_ID"
-      }
+    eas: {
+      projectId: "7282f621-51a5-444f-bca6-955b4499d7a8",
+    },
   },
 };
